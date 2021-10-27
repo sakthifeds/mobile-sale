d280a32e66ba77d9d8abddcfcb62e37acec2a53a
sakthi <sakthi.ramji@hotmail.com>
2021-10-27 15:24:52 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -56,4 +56,13 @@ export class AppComponent implements OnInit {
   
     
   }
+
+   showPreview(event:any){
+    if(event.target.files.length > 0){
+      var src = URL.createObjectURL(event.target.files[0]);
+      console.log('src',src)
+      var preview = document.getElementById("file-ip-1-preview");
+    
+    }
+  }
 }
